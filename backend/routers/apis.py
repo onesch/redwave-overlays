810356818877d76658600a2d97696a1248bdf7ec
onesch <vladimiravakov12345@gmail.com>
2025-07-17 22:54:38 +0400

    HotFix: - Parameters are updatable again (APIs).

--- backend/routers/apis.py
+++ backend/routers/apis.py
@@ -3,17 +3,17 @@ from fastapi import APIRouter
 from backend.services.irsdk_service.service import IRSDKService
 from backend.services.irsdk_service.schemas import IRSDKSchemas
 
-router = APIRouter()
+router = APIRouter(prefix="/api")
 irsdk_service = IRSDKService()
 
 
-@router.get("/api/speed", response_model=IRSDKSchemas)
+@router.get("/speed", response_model=IRSDKSchemas)
 def get_speed_data():
     speed = irsdk_service.get_speed("kmh")
     return IRSDKSchemas(speed=speed)
 
 
-@router.get("/api/controls", response_model=IRSDKSchemas)
+@router.get("/controls", response_model=IRSDKSchemas)
 def get_controls_data():
     throttle = irsdk_service.get_throttle()
     brake = irsdk_service.get_brake()
