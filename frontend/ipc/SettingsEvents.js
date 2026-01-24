39c34be3c8356632c8a8550ef0b5c6ba6b1b814a
onesch <vladimiravakov12345@gmail.com>
2026-01-24 17:53:04 +0400

    fix: added pre-closing of overlay windows to avoid problems.

--- frontend/ipc/SettingsEvents.js
+++ frontend/ipc/SettingsEvents.js
@@ -1,8 +1,15 @@
 const { ipcMain } = require('electron');
 const { resetSettings } = require('../utils/overlay_settings');
+const { overlays } = require('../windows/overlayWindow');
 
 function registerSettingsEvents() {
   ipcMain.on('reset-overlay-settings', () => {
+    // close all overlays
+    for (const overlay of Object.values(overlays)) {
+        if (!overlay.isDestroyed()) overlay.destroy();
+    }
+
+    // delete settings file
     resetSettings();
   });
 }
