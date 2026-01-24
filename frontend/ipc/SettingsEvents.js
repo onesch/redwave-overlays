d4a586a70a35e0f787de30e4e08514440039d859
onesch <vladimiravakov12345@gmail.com>
2026-01-24 18:06:04 +0400

    fix(frontend/utils): preserve zoom in memory to prevent reset on opacity change fix(ipc): add clearZoomCache call in ipc/SettingsEvents.js

--- frontend/ipc/SettingsEvents.js
+++ frontend/ipc/SettingsEvents.js
@@ -1,16 +1,20 @@
 const { ipcMain } = require('electron');
 const { resetSettings } = require('../utils/overlay_settings');
 const { overlays } = require('../windows/overlayWindow');
+const { clearZoomCache } = require('../utils/overlay_zoom');
 
 function registerSettingsEvents() {
   ipcMain.on('reset-overlay-settings', () => {
-    // close all overlays
+    // Сlose all overlays
     for (const overlay of Object.values(overlays)) {
         if (!overlay.isDestroyed()) overlay.destroy();
     }
 
     // delete settings file
     resetSettings();
+    
+    // Clear zoom cache
+    clearZoomCache();
   });
 }
 
